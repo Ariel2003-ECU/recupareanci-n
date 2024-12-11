f71db931c24ae0a432642412d369426479cdebe1
ariel bustos <ariel.bustos@espoch.edu.ec>
2024-12-11 18:12:41 -0500

    commit del medio ciclo

---
+++ src/main/java/estudiante/estructura_de_datos/Estructura_de_datos.java
@@ -0,0 +1,14 @@
+
+package estudiante.estructura_de_datos;
+
+/**
+ *
+ * @author ariel
+ */
+public class Estructura_de_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba de la conexión de repositorio en GitHub");
+        //Hecho por Ariel Bustos
+    }
+}
